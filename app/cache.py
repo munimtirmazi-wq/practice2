6ac13b8b5c2837cb9661121825aec3a82b0a2aab
munimtirmazi-wq <munim.tirmazi@softbuilds.co>
2026-09-07 20:09:17 +0500

    comment added

--- app/cache.py
+++ app/cache.py
@@ -1,5 +1,7 @@
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.inmemory import InMemoryBackend
 
+
+# just a comment to test
 def init_cache():
     FastAPICache.init(InMemoryBackend(), prefix= "practice-cache")
